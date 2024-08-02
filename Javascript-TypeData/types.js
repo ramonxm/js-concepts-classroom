391dc7840f54f2942eb4b49189cd847672666339
Ramon Xavier <ramon1234rxm@gmail.com>
2024-08-01 21:36:47 -0300

    aula de funcoes, loops e condicionais

--- Javascript-TypeData/types.js
+++ Javascript-TypeData/types.js
@@ -1,3 +1,5 @@
+
+// Scope Global
 // Boolean
 
 let aluno = "Luan"
@@ -8,7 +10,7 @@ console.log(avaliacao1 >= 6)
 console.log(ehAprovado, typeof ehAprovado)
 
 // FALSY
-// 0 false undefined null "" NaN 
+// 0 false undefined null NaN "" 
 
 // -> ! Negação
 console.log(!aluno) 
@@ -25,7 +27,6 @@ console.log(6 == '6') // Verifica apenas o valor
 console.log(6 === '6') // Verifica o valor e o tipo (É PREFERÍVEL UTILIZAR DESTA FORMA)
 
 let carro = null;
-
 
 console.log(carro)
 
@@ -48,11 +49,12 @@ const usuario = {
   },
   // Anonymous Function
   verificaSeEhMaiorDeIdade: function () {
-    return usuario.idade >= 18
+    return this.idade >= 18
   },
   // Arrow Function = >
   log: () => {
-    return `Usuário se chama ${usuario.nome}`
+    // this.nome será undefined pq estou acessando o this no escopo global
+    return `Usuário se chama ${this.nome}`
   }
 }
 
@@ -64,8 +66,6 @@ console.log(usuario.log())
 console.log(usuario.verificaSeEhMaiorDeIdade())
 
 // ARRAY
-
-
 // No JS um array pode ser uma lista de qualquer coisa, qualquer tipo de dado.
 const listaDeCompras = [
   'banana',
@@ -84,22 +84,20 @@ console.log(listaDeCompras)
 console.log(listaDeCompras.length) // Capturo o tamanho do array
 
 // Function
-
-
 // criando uma função, definindo uma função (lavarLouça) -> { 
 // criando um escopo de função }
-
-// Function Expression (Named Function)
+// Function Expression (Named Function) -> Função nomeada
 function log(valor) {
-
   console.log(valor)
   return valor
 }
 
+console.log(somar(10, 5))
+
+
 function somar(x,y) {
   return x + y
 }
-
 
 function verificaSeEhMaiorDeIdade(idade) {
   return idade >= 18
@@ -110,4 +108,133 @@ console.log('Ele é maior ou menor de idade?', verificaSeEhMaiorDeIdade(15))
 console.log(somar(10, 5))
 
 log('lorena')
-// console.log(lavarLouca('prato do almoço'))
+
+// Anonymous Function -> Função sem nome que precisa ser atribuída a uma variável, não consegue acessar antes de criar
+const sayMyName = function () {
+  console.log("Ramon");
+};
+
+sayMyName()
+
+// Arrow Function (Função flecha), o this assume baseado no escopo que a função foi criada, não consegue acessar antes de criar
+const fazerAndar = () => {
+  // console.log(this)
+}
+
+console.log('arrow function', fazerAndar())
+
+let increment = 0;
+
+console.log(increment++); // 0
+console.log(increment) // 1
+console.log(++increment) // 1
+
+
+let cont = 0
+
+cont += 2
+cont += 2
+
+console.log(cont)
+
+let pao = true;
+let queijo = false;
+
+// AND && - verdadeiro se todos forem verdadeiros
+console.log(pao && queijo); // saída false
+
+// OR || - verdadeiro se pelo menos um for verdadeiro
+console.log(pao || queijo); // saída true
+
+// NOT ! - negação
+console.log(!pao); // saída false
+
+
+// Se eu acordar 10h da manhã então eu vou jogar futebol, senão eu não volto.
+// SE condição ENTAO faço alguma coisa OU minha mãe me acordou ENTAO faço alguma coisa SENAO faço outra coisa 
+
+const horaDeAcordar = 10
+const horaQueEuAcordei = 11
+
+
+// If ... else
+if (horaQueEuAcordei <= horaDeAcordar) {
+  console.log('Boa, posso ir jogar futebol')
+} else {
+  console.log('Não vou poder ir jogar futebol')
+}
+
+function checarTemperatura(temperatura) {
+  if (temperatura >= 37.5) {
+     // Com o return eu to dizendo pro JS que eu quero parar a execução dessa função
+    return "Febre alta"
+  
+  }
+
+  if (temperatura < 37.5 && temperatura >= 37) {
+    return "Febre moderada"
+  }
+
+  return 'Saudável'
+}
+
+console.log(checarTemperatura(36))
+
+// Switch
+function calculadora(numero1, operador, numero2) {
+  let resultado;
+
+  switch (operador) {
+    case "+":
+      resultado = numero1 + numero2;
+      break;
+    case "-":
+      resultado = numero1 - numero2;
+      break;
+    case "*":
+      resultado = numero1 * numero2;
+      break;
+    case "/":
+      resultado = numero1 / numero2;
+      break;
+    default:
+      console.log("não implementado");
+      break;
+  }
+
+  return resultado;
+}
+
+console.log("+" === "+")
+console.log(calculadora(4, "+", 8));
+
+
+// Looping
+let array = ['', '', '']
+for (let inicializador = 0; inicializador <= array.length; inicializador++) {
+  console.log(inicializador)
+}
+
+// Pega todas as posições
+let nomes = ['João', 'Paulo', 'Pedro']
+for (let nome of nomes){
+  console.log(nome)
+}
+
+for (let indice in nomes){
+  console.log(indice)
+}
+
+// ENQUANTO eu estiver na sala ENTAO eu nao vou dormir 
+
+// While
+let i = 0
+while (i <= 10) {
+  console.log('Ainda estou na sala')
+
+  if (i === 10) {
+    console.log('Finalmente vou dormir')
+  }
+
+  i++
+}
